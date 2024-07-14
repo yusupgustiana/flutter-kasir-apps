b83eb6598fbbcf012faf9d561f76ba5c04c4109d
YusufGustiana <gustiana379@gmail.com>
2024-07-15 00:48:19 +0700

    permission internet

--- lib/core/constants/variable.dart
+++ lib/core/constants/variable.dart
@@ -1,5 +1,5 @@
 class Variable {
-  static String baseUrl = 'http://192.168.1.5:8000';
-  // static String baseUrl = 'https://1731-103-108-130-158.ngrok-free.app ';
-  static String imageBaseUrl = '$baseUrl/storage/public/products/';
+  // static String baseUrl = 'http://192.168.1.5:8000';
+  static String baseUrl = 'http://www.new.kasirax.my.id';
+  static String imageBaseUrl = '$baseUrl/storage/products/';
 }
